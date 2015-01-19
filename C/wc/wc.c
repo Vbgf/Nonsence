dd17ec8ec29485db969017dd354cfd9cbdc2a1c6
Vasil Kolev <vasil.v2@mail.bg>
2015-01-19 19:11:33 +0200

    w

--- C/wc/wc.c
+++ C/wc/wc.c
@@ -8,8 +8,13 @@
 // This file is the source code for the standard wc command in unix. 
 //---------------------------------------------
 
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 //--------------------------------------------
 // FUNCTION: void read_stdin()
@@ -29,8 +34,54 @@ void read_stdin(){
 // char argv - the name of the file that needs to be opened.
 //----------------------------------------------
 
-void read_file(char argv[]){
+int read_file(char argv[]){
 	printf("You just entered read_file() with %s\n", argv);
+	
+	char buff[200];
+	int fd;
+	fd=open(argv, O_RDONLY);
+	if(fd==-1){
+		perror(argv);
+		return 1;
+	}
+	printf("Working on %s\n", argv);
+	int lines, words, chars;
+	int eof = 0;
+	while(!eof){
+		int status_read = read(fd, buff, 200);
+		if(status_read == -1){
+			if(errno==EINTR){
+				status_read = 0;
+			}else{
+				perror(argv);
+				return 1;
+			}
+		}else if(status_read == 0){
+			eof = 1;
+			break;
+		}
+		int written=0;
+		while(written < status_read){
+			int status_write = write(STDOUT_FILENO, buff + written, status_read - written);
+			if(status_write == -1){
+				if(errno == EINTR){
+					status_write = 0;
+				}else{
+					perror(argv);
+					return 1;	
+				}
+			}
+			written += status_write;
+		}
+	}
+	
+	int close_status = close(fd);
+	if(close_status == -1){
+		perror(argv);
+		return 1;
+	}
+	printf("%d %d %d %s\n", lines, words, chars, argv);
+	return 0;
 }
 
 //--------------------------------------------
