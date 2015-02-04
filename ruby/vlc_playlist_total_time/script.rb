44a9e87c003c2a5b79736dcf7195f052d9c4e03e
Vasil Kolev <vasil.v2@mail.bg>
2015-02-04 23:22:14 +0200

    works! vlc_ ...

--- ruby/vlc_playlist_total_time/script.rb
+++ ruby/vlc_playlist_total_time/script.rb
@@ -20,9 +20,35 @@ def check_input_file
 end
 
 def	check_total_time
-
+	total_time = 0
+	f = File.open(ARGV[0], "r")
+	f.each_line do |line|
+		line = line.split("<duration>")[1]
+		if (line != nil)
+			line = line.split("</duration>")[0]
+			total_time = total_time + line.to_i
+		end	
+	end
+	f.close
+	return total_time
 end
 
-if(check_input_file == 0)
-	check_total_time
+def print_time (time)
+	#time is in miliseconds so /1000
+	time = time/1000
+	print "#{time%60} seconds, "
+	time = time/60
+	print "#{time%60} minutes, "
+	time = time/60
+	print "#{time%24} hours, "
+	time = time/24
+	puts "#{time} days"
 end
+
+def main
+	if(check_input_file == 0)
+		print_time (check_total_time)
+	end
+end
+
+main
