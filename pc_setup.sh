caa79a26e35b8cfcf51f153948f735dd5e893da7
Vbgf <vasil.v2@mail.bg>
2015-03-06 02:24:29 +0200

    w/e

---
+++ pc_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+apt-get update
+apt-get upgrade
+apt-get install geany -y
+apt-get install gcc -y
+apt-get install g++ -y
+apt-get install ruby -y
+apt-get install python -y
+apt-get install python-dev -y
+apt-get install libreoffice -y
+apt-get install bundler -y
+apt-get install flashplugin-installer -y
+apt-get install git -y
+cd ~/Desktop
+git clone https://github.com/thebravoman/software_engineering_2014
+git clone https://github.com/vbgf/Nonsence
+git clone https://github.com/vbgf/lazy_slobs
+git clone https://github.com/lchorbadjiev/teach2014-2015
+git clone https://github.com/nikolovd/pps
+clear
+
