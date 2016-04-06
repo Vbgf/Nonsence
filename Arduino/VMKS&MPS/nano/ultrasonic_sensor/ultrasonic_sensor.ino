f3545f5824e8b4314c7abe4f6486c5cbca0374ed
vbgf <vasil.v2@mail.bg>
2016-04-06 21:23:45 +0300

    ultrasonic tests

--- Arduino/VMKS&MPS/nano/ultrasonic_sensor/ultrasonic_sensor.ino
+++ Arduino/VMKS&MPS/nano/ultrasonic_sensor/ultrasonic_sensor.ino
@@ -1,6 +1,8 @@
+#define VCC  10  //D4, +5V
 #define TRIG 11 //D5, digital output
 #define ECHO 12 //D6, analog input
-#define TIMEOUT 10000
+//#define GND   //D7, GND
+#define PWM  3  // test pin
 
 int maximumRange = 200;
 int minimumRange = 0;
@@ -8,9 +10,15 @@ long duration, distance;
 
 void setup() {
   Serial.begin (9600);
+  pinMode(VCC,  OUTPUT);
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT );
+//  pinMode(GND,  OUTPUT);
+
+  digitalWrite(VCC,  HIGH);
   digitalWrite(TRIG, LOW );
+//  digitalWrite(GND,  LOW );
+  analogWrite(PWM, 150);
 }
 
 void loop() { 
@@ -19,7 +27,7 @@ void loop() {
   digitalWrite(TRIG, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG, LOW);
-  duration = pulseIn(ECHO, HIGH, TIMEOUT);
+  duration = pulseIn(ECHO, HIGH);
   
   //Calculate the distance (in cm) based on the speed of sound.
   distance = duration/58.2;
@@ -31,5 +39,5 @@ void loop() {
     Serial.println(distance);
   }
   
-  //delay(100);
+  delay(100);
 }
